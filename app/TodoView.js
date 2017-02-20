58224d3fdd07380e707595015e31bbfc4266bf69
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2017-02-20 23:12:00 +0100

    Fix view script

--- app/TodoView.js
+++ app/TodoView.js
@@ -1,18 +1,5 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-
-
-    let todoList = [
-        new Todo("Take cat to walk"),
-        new Todo("Make dinner"),
-        new Todo("Learn something cool")
-    ];
-
-    TodoRenderer.renderList(todoList);
-
-})
-
 var TodoView = (function (view) {
-    let _todos = [
+    var _todos = [
         new Todo("Take cat to walk"),
         new Todo("Make dinner"),
         new Todo("Learn something cool")
@@ -20,24 +7,37 @@ var TodoView = (function (view) {
 
     view.addTodo = function(){
         alert("Add!");
-    }
+    };
 
     view.deleteTodo = function(id){
         alert("Delete");
         let removedTodo = _findTodo(id);
         _todos.splice(_todos.indexOf(removedTodo),1);
+        reload();
+    };
+
+    view.reload = function(){
+        TodoRenderer.renderList(getTodos());
     }
 
     _findTodo = function(id){
-         _todos.find((todo, index)=> todo.id === id);
-    }
+        return _todos.find((todo, index)=> todo.id === id);
+    };
 
     view.changeState = function(checkbox, id){
         alert("Change!");
         let todo = _findTodo(id);
         todo.isCompleted = checkbox.checked;
-    }
+    };
+
+    view.getTodos = function(){
+        return _todos;
+    };
 
     return view;
 
 })(TodoView || {});
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    TodoRenderer.renderList(TodoView.getTodos());
+});
