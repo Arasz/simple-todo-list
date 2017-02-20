d5fff007576c3bb72d75518170f333604a3e8501
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2017-02-20 22:57:43 +0100

    Add todoView module

--- app/TodoView.js
+++ app/TodoView.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event){
-    
+document.addEventListener("DOMContentLoaded", function (event) {
+
 
     let todoList = [
         new Todo("Take cat to walk"),
@@ -10,3 +10,34 @@ document.addEventListener("DOMContentLoaded", function(event){
     TodoRenderer.renderList(todoList);
 
 })
+
+var TodoView = (function (view) {
+    let _todos = [
+        new Todo("Take cat to walk"),
+        new Todo("Make dinner"),
+        new Todo("Learn something cool")
+    ];
+
+    view.addTodo = function(){
+        alert("Add!");
+    }
+
+    view.deleteTodo = function(id){
+        alert("Delete");
+        let removedTodo = _findTodo(id);
+        _todos.splice(_todos.indexOf(removedTodo),1);
+    }
+
+    _findTodo = function(id){
+         _todos.find((todo, index)=> todo.id === id);
+    }
+
+    view.changeState = function(checkbox, id){
+        alert("Change!");
+        let todo = _findTodo(id);
+        todo.isCompleted = checkbox.checked;
+    }
+
+    return view;
+
+})(TodoView || {});
